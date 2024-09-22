ad67fbd1573e1cfbc0bffa0fe537465828845a51
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-22 18:35:53 +0545

    add docker-compose file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
-const items = require("./items.json");
+const items = require("./data.json");
 
 
 app.use(cors());
